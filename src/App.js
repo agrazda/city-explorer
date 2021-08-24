65a3dd48dfeafc3f19e48bae49fb99b18fc96d19
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-24 15:25:08 -0700

    Lab 06 card 02 completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
-import ExploreButton from './exploreButton';
+import ExploreButton from './ExploreButton';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-
+      citySearch: '',
+      city: {}
     }
   }
+
+  getCity = async (event) => {
+    event.preventDefault()
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.citySearch}&format=json`;
+    const response = await axios.get(API);
+    console.log(response.data[0])
+    this.setState({ city: response.data[0] });
+  }
+
+  updateSearch = (event) => {
+    this.setState({ citySearch: event.target.value})
+    //Function is used for the form - When form is used it will update state(updating the actual city the customer is searching for)
+  }
+
+
   render(){
+    console.log(this.state)
     return(
       <>
-
+        <ExploreButton 
+          updateSearch = {this.updateSearch} 
+          getCity = {this.getCity} 
+          />
+          <h2>The city is: {this.state.city.display_name}</h2>
+          <h3>The Latitude is: {this.state.city.lat}</h3>
+          <h3>The Latitude is: {this.state.city.lon}</h3>
       </>  
     );
   }
---
+++ src/ExploreButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+
+class ExploreButton extends React.Component{
+render(){
+    return(
+        <Form 
+        onSubmit={(event) => this.props.getCity(event)}
+        
+        >
+            <Form.Group 
+                className="formOne" controlId="basicForm"
+            >
+                <Form.Label>Enter the city you want to see</Form.Label>
+                <Form.Control type="city" placeholder="Enter city" onChange={(event) => this.props.updateSearch(event)} /> 
+                {/* onCHange(event) will run for each Key Press in the text field and updateing state in app */}
+            </Form.Group>
+        <Button variant="primary" type="Explore">
+            Explore!
+        </Button>
+        </Form>
+    );
+  }
+}
+
+export default ExploreButton
