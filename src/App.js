7599f8d0286642d8410f7e80bd65fea9154b1247
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-08-24 23:07:01 -0700

    card 4 complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import axios from "axios";
 import ExploreButton from "./ExploreButton";
 import Card from "react-bootstrap/Card";
+import Alert from "react-bootstrap/Alert";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,9 +17,16 @@ class App extends React.Component {
   getCity = async (event) => {
     event.preventDefault();
     const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.citySearch}&format=json`;
-    const response = await axios.get(API);
-    console.log(response.data[0]);
-    this.setState({ city: response.data[0] });
+
+    try {
+      const response = await axios.get(API);
+      this.setState({
+        city: response.data[0],
+        alert: "",
+      });
+    } catch (error) {
+      this.setState({ alert: `${error}` });
+    }
   };
 
   updateSearch = (event) => {
@@ -27,40 +35,50 @@ class App extends React.Component {
   };
 
   render() {
-    console.log(this.state);
     return (
-      // <div>
-      //   <ExploreButton
-      //   updateSearch = {this.updateSearch}
-      //   getCity = {this.getCity}
-      //  />
-      //     <h2>The city is: {this.state.city.display_name}</h2>
-      //     <h3>The Latitude is: {this.state.city.lat}</h3>
-      //     <h3>The Latitude is: {this.state.city.lon}</h3>
-      //     <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.city.lat},${this.state.city.lon}&zoom=12 roundedCircle`} />
-      // </div>
-      <div>  
+      <div>
         <header>City Explorer</header>
-        <Card className= 'customCard' style={{ width: "700px", height: "700px"}}>
-          <Card.Body>
-            <ExploreButton
-              updateSearch={this.updateSearch}
-              getCity={this.getCity}
-            />
-            <Card.Title>
-              <h2>The city is: {this.state.city.display_name}</h2>
-            </Card.Title>
-            <Card.Text>
-              {/* <h2>The city is: {this.state.city.display_name}</h2> */}
-              <h3>The Latitude is: {this.state.city.lat}</h3>
-              <h3>The Latitude is: {this.state.city.lon}</h3>
-            </Card.Text>
-            <img style={{width: 450}}
-              variant="top"
-              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.city.lat},${this.state.city.lon}&zoom=12`}
-            />
-          </Card.Body>
-        </Card>
+        <ExploreButton
+          updateSearch={this.updateSearch}
+          getCity={this.getCity}
+        />
+        {this.state.alert ? (
+          <Alert
+            variant={"danger"}
+            style={{
+              margin: "0px 0px 20px 30px",
+              width: "60%",
+            }}
+          >
+            <Alert.Heading>{this.state.alert}</Alert.Heading>
+            <hr />
+            <p> That is not a valid location. Please enter valid location</p>
+          </Alert>
+        ) : (
+          this.state.city.place_id && (
+            <Card
+              className="customCard"
+              style={{ width: "700px", height: "700px" }}
+            >
+              <Card.Body>
+                <Card.Title>
+                  <h2>The city is: {this.state.city.display_name}</h2>
+                </Card.Title>
+                <Card.Text>
+                  {/* <h2>The city is: {this.state.city.display_name}</h2> */}
+                  <h3>The Latitude is: {this.state.city.lat}</h3>
+                  <h3>The Latitude is: {this.state.city.lon}</h3>
+                </Card.Text>
+                <img
+                  style={{ width: 450 }}
+                  alt="map"
+                  variant="top"
+                  src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.city.lat},${this.state.city.lon}&zoom=12`}
+                />
+              </Card.Body>
+            </Card>
+          )
+        )}
       </div>
     );
   }
